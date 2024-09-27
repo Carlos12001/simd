5e92e290a578a1f8356297be5caa32fa53481d9e
Carlos12001 <carlos.andres12001@gmail.com>
2024-09-27 10:21:55 -0600

    feat: add complete

--- src/compilador/test_ld_escalar.asm
+++ src/compilador/test_ld_escalar.asm
@@ -12,6 +12,16 @@
         addi $t2, $zero, 0
         addi $t3, $zero, 0
         addi $t4, $zero, 0
-        
+
+
+        vset $v5, 2
+        vset $v2, 3
+        vadd $v1, $v5, $v2
+        vst $v5, $zero, 0
+
+
+        addi $t2, $zero, 0
+        addi $t3, $zero, 0
+        addi $t4, $zero, 0
         
 
